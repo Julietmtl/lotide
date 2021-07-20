2a855f8c9d99dcab19196103baf5f2c60e1acbf1
Juliet Li <li_juliet@hotmail.com>
2021-07-20 22:47:52 +0000

    add assertArraysEqual.js and update eqArrays.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  let final = "";
+  if (actual === expected) {
+    final = console.log(`😀😀😀 ${actual} === ${expected}`);
+  } else {
+    final = console.log(`😔😔😔 ${actual} !== ${expected}`);
+  }
+  return final;
+};
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("These two arrays are equal.");
+  } else {
+  console.log("These two arrays are not equal.");
+  }
+}
+
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
--- eqArrays.js
+++ eqArrays.js
@@ -10,13 +10,15 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    } else {
+  if (array1.length !== array2.length) {
     return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
+  return true;
 }
 
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
@@ -24,8 +26,8 @@ const eqArrays = function(array1, array2) {
 
 //example
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false) // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
