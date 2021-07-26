2110ac34bc0ae681442c9951762b13445a178db8
Juliet Li <li_juliet@hotmail.com>
2021-07-26 17:10:10 +0000

    add and refactor assertArraysEqual.js and tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let final = "";
-  if (actual === expected) {
-    final = console.log(`😀😀😀 ${actual} === ${expected}`);
-  } else {
-    final = console.log(`😔😔😔 ${actual} !== ${expected}`);
-  }
-  return final;
-};
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -28,6 +7,9 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`😔😔😔 ${array1} !== ${array2}`);
   }
 };
+
+module.exports = assertArraysEqual;
+
 
 
 //Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
